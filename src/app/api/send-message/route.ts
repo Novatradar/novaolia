9c872c066b00c0b4132f3011d675041b99775ce8
ieadeleke <eadelekeife@yahoo.com>
2025-08-07 12:08:48 +0100

    telegram chat

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,13 +1,13 @@
 // app/api/send-message/route.ts
 import { NextResponse } from 'next/server';
 
-const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN!;
-const CHAT_ID = process.env.TELEGRAM_CHAT_ID!;
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 export async function POST(req: Request) {
-  const { name, email, message, phoneNumber } = await req.json();
+  const { name, email, message, phoneNumber, purpose } = await req.json();
 
-  const text = `📬 New Contact Message:\n\n👤 Name: ${name}\n📧 Email: ${email}\n📧 Phone: ${phoneNumber}\n📝 Message:\n${message}`;
+  const text = `📬 New Contact Message:\n\n👤 Name: ${name}\n📧 Email: ${email}\n📧 Phone: ${phoneNumber}\n📧 Purpose: ${purpose}\n📝 Message:\n${message}`;
 
   const telegramUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 
